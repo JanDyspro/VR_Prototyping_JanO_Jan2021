3d31a7092a15635a2df78c1607532439dff79e2d
Dysprotex <58705183+Dysprotex@users.noreply.github.com>
2021-01-20 15:35:47 +0100

    OpenDoors

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField] private GameObject leftDoor = null;
+    [SerializeField] private GameObject rightDoor = null;
+
+    [SerializeField] private Vector3 targetLocationLeft;
+    [SerializeField] private Vector3 targetLocationRight;
+
+    [Range(1, 10), SerializeField] private float moveDuration = 1;
+    
+    public void OnButtonPress()
+    {
+        StartCoroutine(DoMove());
+    }
+
+    private IEnumerator DoMove()
+    {
+        Debug.Log("Button pressed");
+        leftDoor.transform.DOMove(targetLocationLeft, moveDuration);
+        
+        yield return new WaitForSeconds(moveDuration);
+        rightDoor.transform.DOMove(targetLocationRight, moveDuration);
+    }
+}
---
+++ Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Animation))]
+public class PlayAnimation : MonoBehaviour
+{
+    [SerializeField] private Animation _animation = null;
+    private void Awake()
+    {
+        if (_animation == null)
+        {
+            _animation = GetComponent<Animation>();
+        }
+    }
+
+    public void PlayAnimationClip()
+    {
+        _animation.Play();
+    }
+}
