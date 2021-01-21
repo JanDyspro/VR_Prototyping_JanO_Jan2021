7f54cf86ce35386d0f543b63458aa827804d2986
Dysprotex <58705183+Dysprotex@users.noreply.github.com>
2021-01-21 17:23:48 +0100

    PressurePlate

---
+++ Assets/Scripts/CornerClimb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BNG;
+using DG.Tweening;
+using UnityEngine;
+
+public class CornerClimb : MonoBehaviour
+{
+    [SerializeField] private PressurePlate pressurePlate = null;
+    [SerializeField] private Vector3 targetLocation = Vector3.zero;
+    [SerializeField] private GameObject player = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (pressurePlate.isPressed)
+        {
+            player.transform.DOMove(targetLocation, 1);    
+        }
+    }
+}
---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject door = null;
+
+    [SerializeField] private Vector3 targetPosition = Vector3.zero;
+    private Vector3 startPosition;
+
+    [Range(1, 10), SerializeField] private float moveDuration = 1;
+
+    [HideInInspector] public bool isPressed;
+
+    private void Awake()
+    {
+        startPosition = door.transform.position;
+    }
+
+    public void OpenDoor()
+    {
+        door.transform.DOMove(targetPosition, moveDuration);
+    }
+
+    public void CloseDoor()
+    {
+        door.transform.DOMove(startPosition, moveDuration);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        OpenDoor();
+        isPressed = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CloseDoor();
+        isPressed = false;
+    }
+}
