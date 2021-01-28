8aefa2643fc46e870a484ccf7699742abb5f86e8
Dysprotex <58705183+Dysprotex@users.noreply.github.com>
2021-01-28 04:25:12 +0100

    Hoffentlich Fertig

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
+    [SerializeField] private PlayAnimation _playAnimation = null;
+    [SerializeField] private Animation _animation = null;
+    private void Awake()
+    {
+        if (_animation == null)
+        {
+            _animation = GetComponent<Animation>();
+        }
+    }
     
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Head")
+        {
+            _playAnimation.PlayAnimationClip();
+        }
+        
+    }
 }
